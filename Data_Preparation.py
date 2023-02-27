178fd24ad36b43b2cc1f599edb1464e918177efc
Zaid Ahmed Belwadi <126369399+ZaidAhmedBelwadi@users.noreply.github.com>
2023-02-27 08:35:40 +0530

    Used standard train/test split
    Removed the merging of the standard split and then randomly splitting train-validation-test code before. Instead used the standard split and from the train data itself and then created validation data from the train data itself.

--- Data_Preparation.py
+++ Data_Preparation.py
@@ -11,13 +11,16 @@ def data_preparation(dataset="fashion_mnist"):
     labels = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-  # Merging the standard train/test split 
-  x_data, y_data = np.concatenate((x_train, x_test), axis=0), np.concatenate((y_train, y_test), axis=0)
-  
-  # Randomly shuffle data then split train, test, validatation data (along with normalization)
-  shuffle_indices = np.random.permutation(range(len(y_data)))
-  x_data, y_data = x_data[shuffle_indices,:,:], y_data[shuffle_indices]
-  x_train, x_test, x_val = x_data[:54000]/255, x_data[54000:60000]/255, x_data[60000:]/255
-  y_train, y_test, y_val = y_data[:54000], y_data[54000:60000], y_data[60000:]
+  # Normalizing the data
+  x_train, x_test = x_train/255, x_test/255
+
+  # Randomly shuffle train data then split it into train and validatation data
+  shuffle_indices = np.random.permutation(range(len(y_train))) # Creating random indices for shuffling
+  x_train, y_train = x_train[shuffle_indices,:,:], y_train[shuffle_indices] # Shuffled train data
+
+  val_fraction = 0.1 # 10% of train data to be used for validation
+  len_train = int((1-val_fraction)*len(y_train))  # Length of train data after removing validation from it
+  x_train, x_val = x_train[:len_train], x_train[len_train:]
+  y_train, y_val = y_train[:len_train], y_train[len_train:]
 
   return x_train, x_test, x_val, y_train, y_test, y_val, labels
